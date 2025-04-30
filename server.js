be76afdaf6a0c7b56562aa945bef157437dac689
SakthiR25 <114579047+SakthiR25@users.noreply.github.com>
2025-04-30 16:56:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,10 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
+app.get('/api/test', (req, res) => {
+  console.log("GET /api/test hit");
+  res.json({ message: "API is working" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
