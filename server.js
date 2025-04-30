9a08c800fba96d489f5543491c332bfa787ee4d7
SakthiR25 <114579047+SakthiR25@users.noreply.github.com>
2025-04-30 17:27:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// const app = express();
-
-// // Load env variables
-// dotenv.config();
-
-// // Middleware
-// app.use(express.json());
-
-// // DB Connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => console.log('MongoDB connected'))
-// .catch((err) => console.log(err));
-
-// // Routes
-// app.use('/api/auth', require('./routes/auth'));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// const app = express();
-
-// // Load env variables
-// dotenv.config();
-
-// // Middleware
-// app.use(express.json());
-
-// // DB Connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => console.log('MongoDB connected'))
-// .catch((err) => console.log(err));
-
-// // Routes
-// app.use('/api/auth', require('./routes/auth'));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -73,8 +21,8 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-app.get('/api/hello', (req, res) => {
-  res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
 
