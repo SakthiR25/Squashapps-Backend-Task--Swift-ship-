3d1103c6dc6972a89cb85a8ec0d232c5bee2eb3d
SakthiR25 <114579047+SakthiR25@users.noreply.github.com>
2025-04-30 18:58:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ dotenv.config();
 
 // Middleware
 app.use(express.json());
-    origin: ['http://localhost:5173', 'https://spiffy-cucurucho-832381.netlify.app'], // Include your Netlify frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-    allowedHeaders: 'Content-Type,Authorization',
+app.use(cors({
+  origin: "https://spiffy-cucurucho-832381.netlify.app", // Frontend URL
+  credentials: true // Allow cookies to be sent with the request
 }));
 
 
